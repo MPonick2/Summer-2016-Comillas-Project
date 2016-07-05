541f9239b1def3eb35ca435500f3c1d02185487b
Theresa Ponicki <Theresa Ponicki>
2016-07-05 16:15:13 -0500

    initial commit of village code sorting

---
+++ Village_code_sorting.py
@@ -0,0 +1,18 @@
+import csv
+
+def read_files(DCHB_Village_Release_1000):
+	village_info=csv.reader(open(DCHB_Village_Release_1000,'rb'))
+	
+	for row in village_info:
+	
+
+
+matching_town_codes = {}
+nonmatching_town_codes = {}
+
+for community in district:
+	if town_code_QGIS==village_code_excel:
+		matching_town_codes.append(village_code_excel)
+	else:
+		nonmatching_town_codes.append(village_code_excel)
+		
